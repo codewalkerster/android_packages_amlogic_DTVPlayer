956c99123f7d5a40df8083663a27d7f4ab16b48e
ying.liu <ying.liu@amlogic.com>
2014-09-01 10:24:55 +0800

    PD #96847 Add libraries into apk package.
    Change-Id: If9737b5542cbfca84f9e77c62c1818abdb02e505

--- Android.mk
+++ Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 #LOCAL_SDK_VERSION := current
 LOCAL_PACKAGE_NAME := DTVPlayer
 LOCAL_STATIC_JAVA_LIBRARIES := tvmiddleware
-
+LOCAL_JNI_SHARED_LIBRARIES := libam_adp libam_mw libjnitvmboxdevice libjnitvdatabase libjnitvscanner libjnitvsubtitle libjnitvepgscanner libzvbi
 LOCAL_PROGUARD_ENABLED := disabled
 
 LOCAL_CERTIFICATE := platform
